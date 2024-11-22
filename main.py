8b9fdb9872f87f6bb554e82ddfa52b4f01fe806d
3ef1rus <ivanvyal321@gmail.com>
2024-11-23 00:41:07 +0300

    reworked init ,add show_book for view info about book, add change_status in class Book; create load info about books from json, add annotations , add log in console, change status book by id in class Library;

--- main.py
+++ main.py
@@ -1,41 +1,106 @@
 import uuid
-import typing
-from data import save_books_to_json
+from time import perf_counter
+from typing import List, Optional
+
+from data import save_books_to_json, read_from_file
+
+
 class Book:
-    def __init__(self,title, author, year):
-        self.id:uuid.UUID.hex=uuid.uuid4().hex
-        self.title:str = title
-        self.author:str = author
-        self.year:int = year
-        self.status:bool = True
+    def __init__(self,
+                 title: str,
+                 author: str,
+                 year: int,
+                 id:Optional[uuid.UUID] = None,
+                 status:Optional[bool] = None
+                 ) -> None:
+        self.title = title
+        self.author = author
+        self.year = year
+        if id is None:
+            self.id:uuid.UUID  = uuid.uuid4().hex
+        else:
+            self.id = id
+        if status is None:
+            self.status: bool = True
+        else:
+            self.status = status
+
+    def show_book(self):
+        status="В наличии" if self.status is True else "Выдана"
+        print(f"id книги: {self.id},"
+              f" Название книги: {self.title},"
+              f" Автор книги: {self.author},"
+              f" Год издания: {self.year},"
+              f" Состояние книги: {status}")
+
+
+    def change_status(self, status:bool):
+        self.status = status
 
 
 class Library:
 
-    def __init__(self):
-        self.books:typing.List[Book] = []
+    def __init__(self)->None:
+        self.books: List[Book] = []
 
-    def add_book(self,title,author,year):
-        self.books.append(Book(title,author,year))
+    def load_books_from_file(self)->None:
+        data = read_from_file('template_books.json')
+        for book_data in data:
+            book = Book(title=book_data["title"],
+                        author=book_data["author"],
+                        year=book_data["year"],
+                        id=book_data["id"],
+                        status=book_data["status"]
+                        )
+            self.books.append(book)
 
-    def delete_book(self,book_id):
+    def add_book(
+            self,
+            title:str,
+            author:str,
+            year:int
+    )->None:
+        self.books.append(Book(title, author, year))
+
+    def delete_book(self, book_id:uuid.UUID)->None:
         for book in self.books:
             if book.id == book_id:
                 self.books.remove(book)
+                print("Книга удалена")
+                return None
+        print(f"Книги с id {book_id} не существует")
 
-books = [
-    Book("Война и мир", "Лев Толстой", year=1869),
-    Book("Преступление и наказание", "Фёдор Достоевский", year=1866),
-    Book("Мастер и Маргарита", "Михаил Булгаков", year=1967),
-    Book("Сто лет одиночества", "Габриэль Гарсиа Маркес", year=1967),
-    Book("1984", "Джордж Оруэлл", 1949),
-    Book("Божественная комедия", "Данте Алигьери", year=1321),
-    Book("Гордость и предубеждение", "Джейн Остин", year=1813),
-    Book("Лолита", "Владимир Набоков", year=1955),
-    Book("Моби Дик", "Герман Мелвилл", year=1851),
-    Book("Маленький принц", "Антуан де Сент-Экзюпери", year=1943),
-]
-
-save_books_to_json(books, 'template_books.json')
+    def show_all_books(self):
+        for book in self.books:
+            book.show()
 
 
+    def change_status_book(self, book_id:uuid.UUID,status:str)->None:
+        data=True if status=="В наличии" else False
+        for book in self.books:
+            if book.id == book_id:
+                book.change_status(True)
+                print("")
+                return True
+        print(f"Книги с id {book_id} не существует")
+
+# books = [
+#     Book("Война и мир", "Лев Толстой", year=1869),
+#     Book("Преступление и наказание", "Фёдор Достоевский", year=1866),
+#     Book("Мастер и Маргарита", "Михаил Булгаков", year=1967),
+#     Book("Сто лет одиночества", "Габриэль Гарсиа Маркес", year=1967),
+#     Book("1984", "Джордж Оруэлл", 1949),
+#     Book("Божественная комедия", "Данте Алигьери", year=1321),
+#     Book("Гордость и предубеждение", "Джейн Остин", year=1813),
+#     Book("Лолита", "Владимир Набоков", year=1955),
+#     Book("Моби Дик", "Герман Мелвилл", year=1851),
+#     Book("Маленький принц", "Антуан де Сент-Экзюпери", year=1943),
+# ]
+# save_books_to_json(books, 'template_books.json')
+library = Library()
+library.load_books_from_file()
+# library.add_book("Гарри Поттер и философский камень","Дж.К. Роулинг","1997")
+library.show_all_books()
+library.delete_book("547d13d9edc24cc9b4fe0478fe59a235")
+library.delete_book("886dd68ff2ff478888a8e0235cd8f655")
+library.show_all_books()
