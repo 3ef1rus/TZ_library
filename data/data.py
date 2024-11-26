9a291ce7d98424bbfe4ba34751c41314104a7e75
3ef1rus <ivanvyal321@gmail.com>
2024-11-26 23:07:24 +0300

    added error and exception handling

--- data/data.py
+++ data/data.py
@@ -1,10 +1,16 @@
 import json
 
 def read_from_file(filename:str)->list:
-    with open(filename, 'r', encoding='utf-8') as f:
-        json_data = json.load(f)
-    return json_data
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+        return json_data
+    except Exception as e:
+        print(f"Произошла ошибка при работе с JSON-данных: {e}")
 
 def save_books_to_json(data:list[dict], filename:str)->None:
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"Произошла ошибка при работе с JSON-данных: {e}")
