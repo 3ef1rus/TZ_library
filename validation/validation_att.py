716f16a4ab16fe3ad04062be41bfdbbc9f340281
3ef1rus <ivanvyal321@gmail.com>
2024-11-26 21:01:20 +0300

    changes in functions

--- validation/validation_att.py
+++ validation/validation_att.py
@@ -1,8 +1,25 @@
-def validation_title(title)->bool:
-    pass
+import re
 
-def validation_author(author)->bool:
-    pass
+def validation_title(title) -> bool:
+    if not title:
+        print("Поле не может быть пустым")
+        return False
 
-def validation_year(year)->bool:
-    pass
+
+def validation_author(author) -> bool:
+    if not author:
+        print("Поле не может быть пустым")
+        return False
+
+    pattern = r'^[а-яА-Я\s-]+$'
+    return bool(re.match(pattern, author))
+
+
+def validation_year(year) -> bool:
+    if not year:
+        print("Поле не может быть пустым")
+        return False
+
+    if not year.isdigit():
+        print("Поле не может содержать символы")
+        return False
